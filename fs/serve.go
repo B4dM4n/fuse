7a0ef5fd520d9b41b778e51e057cb4a2403c6030
Fabian Möller <fabianm88@gmail.com>
2019-01-15 21:37:03 +0100

    Add ioctl support

--- fs/serve.go
+++ fs/serve.go
@@ -250,6 +250,10 @@ type NodeRemovexattrer interface {
 	Removexattr(ctx context.Context, req *fuse.RemovexattrRequest) error
 }
 
+type NodeIoctler interface {
+	Ioctl(ctx context.Context, req *fuse.IoctlRequest, resp *fuse.IoctlResponse) error
+}
+
 var startTime = time.Now()
 
 func nodeAttr(ctx context.Context, n Node, attr *fuse.Attr) error {
@@ -1579,6 +1583,20 @@ func (c *Server) handleRequest(ctx context.Context, node Node, snode *serveNode,
 
 		// fallback to always claim ready
 		s.REvents = fuse.DefaultPollMask
+		done(s)
+		r.Respond(s)
+		return nil
+
+	case *fuse.IoctlRequest:
+		n, ok := node.(NodeIoctler)
+		if !ok {
+			return fuse.ENOSYS
+		}
+		s := &fuse.IoctlResponse{}
+		err := n.Ioctl(ctx, r, s)
+		if err != nil {
+			return err
+		}
 		done(s)
 		r.Respond(s)
 		return nil
